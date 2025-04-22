d50b71270e63c1c365403d034890040afb8f5d10
r0cky-j0nes <163677460+r0cky-j0nes@users.noreply.github.com>
2025-04-22 01:46:30 -0400

    update to gh
    adding correct code to add app to gh pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: '/jones_web125_weatherapp/'
+}
+
